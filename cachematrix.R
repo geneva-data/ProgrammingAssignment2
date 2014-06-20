70fc967b00364eb607938113848fd5204a451398
Jonathan Lewis <trellis.ny@gmail.com>
2014-06-20 11:39:26 -0400

    Coursera - Programming Assignment Submission
    Made some additional comments to clarify methodolgy and highlight
    results of the code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,14 @@
 ## Coursera Data Science - R Programming Assignment 2
+##*****************************************************
+##
+## Methodology I used to complete this assignment was to take the CacheMean example provided in the assignment description and
+## change the vector type to matrix and sustitute the references to mean command with the inverse command
+##
+##***************************************************
+##
+## Results of the execution of this code were pasted below the code as comments
+##
+##****************************************************
 ## Caching the Inverse of a matrix in order to save computation time
 ## The first function, makecachematrix creates a special "vector", which is really a list containing a function to
 ## set the value of the vector
@@ -45,7 +55,7 @@ cacheSolve <- function(x, ...) {
 ## 1) created a matrix x<-matrix(c(2,4,1,3,2,4,5,1,2),3,3)
 ## 2) Solve(x)
 
-##[,1]        [,2]       [,3]
+##           [,1]        [,2]       [,3]
 ##[1,]  0.0000000  0.28571429 -0.1428571
 ##[2,] -0.1428571 -0.02040816  0.3673469
 ##[3,]  0.2857143 -0.10204082 -0.1632653
